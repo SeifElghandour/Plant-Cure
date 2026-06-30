24c30cfcd12ec4260af552b07013cab60575f055
Seif EL-Ghandour <124669935+SeifElghandour@users.noreply.github.com>
2026-07-01 01:57:27 +0300

    changing name3

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5500',
   'http://localhost:8080',
   'http://127.0.0.1:8080',
-  'https://plant-cure-tan.vercel.app',
+  'https://plant-care-tan.vercel.app',
   'null',
 ];
 
